b4b6ca0b260cc0a1003869d855dee34863691af9
Josue Samba <josuesamba99@gmail.com>
2026-05-28 12:54:35 +0200

    add: ex0

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,29 @@
+import typing
+import sys
+
+
+def read_file() -> None:
+    if(len(sys.argv) != 2):
+        print("Usage: ft_ancient_text.py <file>")
+    else:
+        print("=== Cyber Archives Recovery ===")
+        print(f"Accessing file '{sys.argv[1]}'")
+        try:
+            f = open(f"{sys.argv[1]}", "r")
+        except OSError as e:
+            print(f"Error opening file '{sys.argv[1]}': {e}")
+            return
+        content = f.read()
+        f.close()
+        print("---")
+        print()
+        print(f"{content}")
+        print("---")
+        print(f"File '{sys.argv[1]}' closed.")
+
+def main() -> None:
+    read_file()
+
+
+if __name__ == "__main__":
+    main()
